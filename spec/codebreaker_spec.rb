4ff42972ccfa25b182520f2123277dd1f8a0e3fd
vincenthoong <hoongv@gmail.com>
2013-04-29 20:22:47 +0100

    first commit

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -3,17 +3,18 @@ require_relative '../lib/codebreaker'
   describe 'Game' do
   	describe "#start" do
   		before(:each) do
-  			output = double('output').as_null_object
-				game = Game.new(output)
+  			@output = double('output').as_null_object
+				@game = Game.new(@output)
+			end
 
 			it "sends a welcome message" do
-				output.should_receive(:puts).with('Welcome to Codebreaker!')
-				game.start
+				@output.should_receive(:puts).with('Welcome to Codebreaker!')
+				@game.start
 			end
 
 			it "prompts for the first guess" do
-				output.should_receive(:puts).with('Enter guess: ')
-				game.start
+				@output.should_receive(:puts).with('Enter guess: ')
+				@game.start
 			end
 		end
 	end
